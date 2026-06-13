d0eab358198cb2d8faab18725030fedd02b44ce2
Jjapageti <151534275+Jjapageti@users.noreply.github.com>
2026-06-13 23:10:28 +0900

    Add files via upload

---
+++ church-treasure-ready/model/people3/README.txt
@@ -0,0 +1,1 @@
+Teachable Machine TensorFlow.js 모델 파일(model.json, metadata.json, weights.bin)을 이 폴더에 넣으세요.
---
+++ church-treasure-ready/questions/question6.js
@@ -0,0 +1,1 @@
+export default { id:'q6', type:'quiz', title:'성경 퀴즈 6', question:'예수님이 태어나신 곳은 어디일까요?', answers:['베들레헴'] };
